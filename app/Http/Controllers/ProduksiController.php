46c1710bf73159868e1220033939853c9cae7dab
thomasalbertoDev <12221355@bsi.ac.id>
2024-12-14 12:00:16 +0700

    feat: get, filter data produksi completed

--- app/Http/Controllers/ProduksiController.php
+++ app/Http/Controllers/ProduksiController.php
@@ -2,17 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kebun;
 use App\Models\Produksi;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class ProduksiController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    protected function applyFilters(Builder $query, Request $request)
     {
-        return view('pages.admin.produksi.index');
+        $filters = $request->only([
+            'lokasi_kebun',
+            'luas_kebun_mulai',
+            'luas_kebun_selesai',
+            'jumlah_tandan_mulai',
+            'jumlah_tandan_selesai',
+            'berat_total_mulai',
+            'berat_total_selesai',
+            'tanggal_panen_mulai',
+            'tanggal_panen_selesai'
+        ]);
+
+        foreach ($filters as $filterName => $filterValue) {
+            if (!empty($filterValue)) {
+                match ($filterName) {
+                    'lokasi_kebun' => $query->whereHas('kebun', function ($q) use ($filterValue) {
+                        $q->where('lokasi', 'like', '%' . $filterValue . '%');
+                    }),
+                    'luas_kebun_mulai' => $query->whereHas('kebun', function ($q) use ($filterValue) {
+                        $q->where('luas', '>=', $filterValue);
+                    }),
+                    'luas_kebun_selesai' => $query->whereHas('kebun', function ($q) use ($filterValue) {
+                        $q->where('luas', '<=', $filterValue);
+                    }),
+                    'jumlah_tandan_mulai' => $query->where('jumlah_tandan', '>=', $filterValue),
+                    'jumlah_tandan_selesai' => $query->where('jumlah_tandan', '<=', $filterValue),
+                    'berat_total_mulai' => $query->where('berat_total', '>=', $filterValue),
+                    'berat_total_selesai' => $query->where('berat_total', '<=', $filterValue),
+                    'tanggal_panen_mulai' => $query->where('tanggal_panen', '>=', $filterValue),
+                    'tanggal_panen_selesai' => $query->where('tanggal_panen', '<=', $filterValue),
+                };
+            }
+        }
+    }
+
+    /**
+     * Fungsi untuk menampilkan halaman produksi, menerapkan fitur filter, menerapkan fitur pagination dan mengirimkan data lokasi kebun berdasarkan id kebun
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function index(): View
+    {
+        $per_page = request()->input('per_page', 10);
+        $query = Produksi::query()->orderBy('created_at', 'desc');
+
+        $this->applyFilters($query, request());
+
+        $produksi = $query->paginate($per_page)->withQueryString();
+        $lokasi_kebun = Kebun::select('id', 'lokasi')->get();
+
+        return view('pages.admin.produksi.index', [
+            'data' => $produksi->items(),
+            'pagination' => $produksi,
+            'lokasi_kebun' => $lokasi_kebun
+        ]);
     }
 
     /**
--- resources/views/pages/admin/produksi/index.blade.php
+++ resources/views/pages/admin/produksi/index.blade.php
@@ -6,7 +6,314 @@
 
 
 @section('content')
-  <div>
-    <h1>Produksi</h1>
+  <div class="row">
+    <div class="col-xl-12">
+      <div class="card">
+        <div class="card-header d-lg-flex justify-content-between align-items-center gap-1">
+          <h4 class="card-title flex-grow-1">Daftar Produksi</h4>
+          <div class="d-flex flex-wrap  gap-1 mt-lg-0 mt-3">
+            {{-- Button Tambah Produksi --}}
+            <a href="{{ route('admin.produksi.create') }}" class="btn btn-md btn-primary">
+              <iconify-icon icon="ic:baseline-plus" class="align-middle fs-18">
+              </iconify-icon>
+              Tambah Produksi
+            </a>
+
+            {{-- Button Modal --}}
+            <button type="button"
+              class="btn btn-md d-flex gap-1 justify-content-between align-items-center btn-success"
+              data-bs-toggle="modal" data-bs-target="#filterModal">
+              <iconify-icon icon="mage:filter" class="align-middle fs-18">
+              </iconify-icon>
+              Filter
+            </button>
+
+            {{-- Button Refresh Halaman --}}
+            <a href="{{ route('admin.produksi.index') }}" class="btn btn-md btn-secondary">
+              <iconify-icon icon="ic:baseline-refresh" class="align-middle fs-18">
+              </iconify-icon>
+              Refresh
+            </a>
+
+            {{-- Modal Filter Start --}}
+            <div class="modal fade" id="filterModal" tabindex="-1" aria-labelledby="filterModalLabel"
+              aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered modal-lg">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="filterModalLabel">Filter Data Produksi</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                      aria-label="Close"></button>
+                  </div>
+
+                  {{-- Modal Body Start --}}
+                  <div class="modal-body">
+                    <form action="{{ route('admin.produksi.index') }}" method="GET">
+                      {{-- Lokasi Kebun --}}
+                      <div class="mb-3">
+                        <label class="form-label" for="lokasi_kebun">Lokasi Kebun</label>
+                        <select name="lokasi_kebun" id="lokasi_kebun" class="form-control"
+                          aria-label="Pilih Pengguna" data-choices data-choices-search-true
+                          data-choices-removeItem>
+                          <option value="">-- Pilih Lokasi Kebun --</option>
+                          @foreach ($lokasi_kebun as $lokasi)
+                            <option value="{{ $lokasi->lokasi }}"
+                              {{ request()->get('lokasi_kebun') == $lokasi->lokasi ? 'selected' : '' }}>
+                              {{ $lokasi->lokasi }}
+                            </option>
+                          @endforeach
+                        </select>
+                      </div>
+
+                      {{-- Luas Kebun --}}
+                      <div class="d-flex gap-2">
+                        <div class="mb-3 w-100">
+                          <label class="form-label" for="luas_kebun_mulai">
+                            Luas Kebun Mulai
+                          </label>
+                          <input type="number" id="luas_kebun_mulai" class="form-control"
+                            name="luas_kebun_mulai" placeholder="Masukkan Luas Kebun Mulai"
+                            value="{{ request()->get('luas_kebun_mulai') }}">
+                        </div>
+                        <div class="mb-3 w-100">
+                          <label class="form-label" for="luas_kebun_selesai">
+                            Luas Kebun Selesai
+                          </label>
+                          <input type="number" id="luas_kebun_selesai" class="form-control"
+                            name="luas_kebun_selesai" placeholder="Masukkan Luas Kebun Selesai"
+                            value="{{ request()->get('luas_kebun_selesai') }}">
+                        </div>
+                      </div>
+
+
+                      {{-- Jumlah Tandan --}}
+                      <div class="d-flex gap-2">
+                        <div class="mb-3 w-100">
+                          <label class="form-label" for="jumlah_tandan_mulai">
+                            Jumlah Tandan Mulai
+                          </label>
+                          <input type="number" id="jumlah_tandan_mulai" class="form-control"
+                            name="jumlah_tandan_mulai" placeholder="Masukkan Jumlah Tandan Mulai"
+                            value="{{ request()->get('jumlah_tandan_mulai') }}">
+                        </div>
+                        <div class="mb-3 w-100">
+                          <label class="form-label" for="jumlah_tandan_selesai">
+                            Jumlah Tandan Selesai
+                          </label>
+                          <input type="number" id="jumlah_tandan_selesai" class="form-control"
+                            name="jumlah_tandan_selesai" placeholder="Masukkan Jumlah Tandan Selesai"
+                            value="{{ request()->get('jumlah_tandan_selesai') }}">
+                        </div>
+                      </div>
+
+                      {{-- Berat Total --}}
+                      <div class="d-flex gap-2">
+                        <div class="mb-3 w-100">
+                          <label class="form-label" for="berat_total_mulai">
+                            Berat Total Mulai
+                          </label>
+                          <input type="number" id="berat_total_mulai" class="form-control"
+                            name="berat_total_mulai" placeholder="Masukkan Berat Total Mulai"
+                            value="{{ request()->get('berat_total_mulai') }}">
+                        </div>
+                        <div class="mb-3 w-100">
+                          <label class="form-label" for="berat_total_selesai">
+                            Berat Total Selesai
+                          </label>
+                          <input type="number" id="berat_total_selesai" class="form-control"
+                            name="berat_total_selesai" placeholder="Masukkan Berat Total Selesai"
+                            value="{{ request()->get('berat_total_selesai') }}">
+                        </div>
+                      </div>
+
+                      {{-- Tanggal Panen --}}
+                      <div class="d-flex gap-2">
+                        <div class="mb-3 w-100">
+                          <label class="form-label" for="tanggal_panen_mulai">
+                            Tanggal Panen Mulai
+                          </label>
+                          <input type="date" id="tanggal_panen_mulai" class="form-control"
+                            placeholder="-- Pilih Tanggal --" name="tanggal_panen_mulai"
+                            value="{{ request()->get('tanggal_panen_mulai') }}">
+                        </div>
+                        <div class="mb-3 w-100">
+                          <label class="form-label" for="tanggal_panen_selesai">
+                            Tanggal Panen Selesai
+                          </label>
+                          <input type="date" id="tanggal_panen_selesai" class="form-control"
+                            placeholder="-- Pilih Tanggal --" name="tanggal_panen_selesai"
+                            value="{{ request()->get('tanggal_panen_selesai') }}">
+                        </div>
+                      </div>
+
+
+                      {{-- Button Submit --}}
+                      <div class="w-100 d-flex gap-1 justify-content-end">
+                        <button type="button" class="btn btn-secondary"
+                          data-bs-dismiss="modal">Tutup</button>
+                        <button type="submit" class="btn btn-primary">Cari Data Produksi</button>
+                      </div>
+                    </form>
+                  </div>
+                  {{-- Modal Body End --}}
+                </div>
+              </div>
+            </div>
+            {{-- Modal Filter End --}}
+          </div>
+        </div>
+
+        {{-- Table Start --}}
+        <div class="table-responsive">
+          <table class="table align-middle mb-0 table-hover table-centered">
+
+            {{-- Table Head Start --}}
+            <thead class="bg-light-subtle">
+              <tr>
+                <th class="px-4">No</th>
+                <th>Lokasi Kebun</th>
+                <th>Luas Kebun</th>
+                <th>Jumlah Tandan</th>
+                <th>Berat Total</th>
+                <th>Tanggal Panen</th>
+              </tr>
+            </thead>
+            {{-- Table Head End --}}
+
+            {{-- Table Body Start --}}
+            <tbody>
+              @if (empty($data))
+                <tr>
+                  <td colspan="6" class="text-center">
+                    <img src="/images/404-error.png" alt="Not Found Img" class="w-25 h-25">
+                    <h4 class="fw-bold">Data Tidak Ditemukan</h4>
+                  </td>
+                </tr>
+              @else
+                @foreach ($data as $index => $produksi)
+                  <tr>
+                    {{-- No --}}
+                    <td class="px-4">
+                      {{ $loop->iteration + ($pagination->currentPage() - 1) * $pagination->perPage() }}
+                    </td>
+
+                    {{-- Lokasi Kebun --}}
+                    <td> {{ $produksi->kebun->lokasi }} </td>
+
+                    {{-- Luas Kebun --}}
+                    <td>{{ number_format($produksi->kebun->luas, 2) }} m<sup>2</sup></td>
+
+                    {{-- Jumlah Tandan --}}
+                    <td> {{ $produksi->jumlah_tandan }} </td>
+
+                    {{-- Berat Total --}}
+                    <td> {{ $produksi->jumlah_tandan }} </td>
+
+                    {{-- Tanggal Panen --}}
+                    <td>{{ $produksi->tanggal_panen->format('Y-m-d') }}</td>
+
+
+                    {{-- Button Aksi --}}
+                    <td>
+                      <div class="d-flex gap-2">
+                        <a href="{{ route('admin.produksi.edit', [$produksi->id]) }}"
+                          class="btn btn-soft-primary btn-sm" data-bs-toggle="tooltip"
+                          data-bs-placement="top" data-bs-title="Edit Data">
+                          <iconify-icon icon="solar:pen-2-broken" class="align-middle fs-18">
+                          </iconify-icon>
+                        </a>
+                        <button type="button" class="btn btn-soft-danger btn-sm"
+                          data-bs-toggle="modal" data-bs-target="#hapusDataModal">
+                          <iconify-icon icon="solar:trash-bin-minimalistic-2-broken"
+                            class="align-middle fs-18">
+                          </iconify-icon>
+                        </button>
+                      </div>
+                    </td>
+
+                    {{-- Modal Hapus Data Start --}}
+                    <div class="modal fade" id="hapusDataModal" tabindex="-1"
+                      aria-labelledby="hapusDataModalLabel" aria-hidden="true">
+                      <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                          {{-- Modal Body Start --}}
+                          <div class="modal-body">
+                            <form action="{{ route('admin.produksi.destroy', $produksi->id) }}"
+                              method="POST">
+                              @csrf
+                              @method('DELETE')
+
+                              <div class="text-center">
+                                <img src="/images/delete.png" alt="Not Found Img"
+                                  class="w-100 h-100">
+                                <h4 class="fw-bold">Hapus Data Produksi</h4>
+                                <p class="text-muted">
+                                  Anda yakin ingin menghapus data produksi ini?
+                                </p>
+                              </div>
+
+                              {{-- Button Submit --}}
+                              <div class="w-100 d-flex gap-2 justify-content-center mt-3">
+                                <button type="button" class="btn btn-secondary w-100 py-2"
+                                  data-bs-dismiss="modal">Tutup</button>
+                                <button type="submit" class="btn btn-primary w-100 py-2">Hapus
+                                  Data</button>
+                              </div>
+                            </form>
+                          </div>
+                          {{-- Modal Body End --}}
+                        </div>
+                      </div>
+                    </div>
+
+                    {{-- Modal Hapus Data End --}}
+                  </tr>
+                @endforeach
+              @endif
+            </tbody>
+            {{-- Table Body End --}}
+          </table>
+        </div>
+        {{-- Table End --}}
+
+        {{-- Pagination Start --}}
+        <div class="card-footer border-top">
+          <nav aria-label="Page navigation example">
+            <ul class="pagination justify-content-lg-end justify-content-center mb-0">
+              {{-- Navigation ke halaman sebelumnya --}}
+              @if ($pagination->currentPage() > 1)
+                <li class="page-item">
+                  <a class="page-link" href="{{ $pagination->previousPageUrl() }}">Previous</a>
+                </li>
+              @else
+                <li class="page-item disabled">
+                  <a class="page-link" href="#">Previous</a>
+                </li>
+              @endif
+
+              {{-- Looping link halaman --}}
+              @foreach (range(1, $pagination->lastPage()) as $page)
+                <li class="page-item {{ $pagination->currentPage() == $page ? 'active' : '' }}">
+                  <a class="page-link"
+                    href="{{ $pagination->url($page) }}">{{ $page }}</a>
+                </li>
+              @endforeach
+
+              {{-- Link ke halaman berikutnya --}}
+              @if ($pagination->hasMorePages())
+                <li class="page-item">
+                  <a class="page-link" href="{{ $pagination->nextPageUrl() }}">Next</a>
+                </li>
+              @else
+                <li class="page-item disabled">
+                  <a class="page-link" href="#">Next</a>
+                </li>
+              @endif
+            </ul>
+          </nav>
+        </div>
+        {{-- Pagination End --}}
+      </div>
+    </div>
   </div>
 @endsection
