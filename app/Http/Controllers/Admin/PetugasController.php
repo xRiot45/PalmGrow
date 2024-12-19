8f62e87d538accc22776a1d5c4357879b3f4e69f
thomasalbertoDev <12221355@bsi.ac.id>
2024-12-19 20:59:12 +0700

    refactor: refactor code

--- app/Http/Controllers/Admin/PetugasController.php
+++ app/Http/Controllers/Admin/PetugasController.php
@@ -14,16 +14,9 @@ use Illuminate\Http\Request;
 
 class PetugasController extends Controller
 {
-    /**
-     * Fungsi untuk fitur filter
-     * @param \Illuminate\Database\Eloquent\Builder $query
-     * @param \Illuminate\Http\Request $request
-     * @return void
-     */
     private function applyFilters(Builder $query, Request $request): void
     {
         $filters = $request->only(['nama_petugas', 'jabatan', 'tanggal_bergabung_mulai', 'tanggal_bergabung_selesai']);
-
         foreach ($filters as $filterName => $filterValue) {
             if (!empty($filterValue)) {
                 match ($filterName) {
@@ -36,11 +29,6 @@ class PetugasController extends Controller
         }
     }
 
-    /**
-     * Fungsi untuk menampilkan halaman kebun yang disertai dengan fitur filter dan pagination
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Contracts\View\View
-     */
     public function index(Request $request): View
     {
         $perPage = $request->input('perPage', 10);
@@ -49,7 +37,6 @@ class PetugasController extends Controller
         $this->applyFilters($query, $request);
 
         $petugas = $query->paginate($perPage)->appends($request->except('page'));
-
         return view('pages.admin.petugas.index', [
             'data' => $petugas->items(),
             'pagination' => $petugas,
@@ -57,10 +44,6 @@ class PetugasController extends Controller
         ]);
     }
 
-    /**
-     * Fungsi untuk menampilkan halaman tambah petugas dan mengirimkan data pengguna
-     * @return \Illuminate\Contracts\View\View
-     */
     public function create(): View
     {
         $pengguna = Pengguna::whereDoesntHave('petugas')->where('role', 'Petugas Kebun')->get();
@@ -69,23 +52,17 @@ class PetugasController extends Controller
         ]);
     }
 
-    /**
-     * Fungsi untuk menambahkan data petugas
-     * @param \App\Http\Requests\PetugasRequest $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
     public function store(PetugasRequest $request): RedirectResponse
     {
-        Petugas::create($request->only(['pengguna_id', 'nama_petugas', 'jabatan', 'tanggal_bergabung']));
-        return redirect()->route('admin.petugas.index')->with('success', 'Petugas berhasil ditambahkan');
+        $petugas = Petugas::create($request->validated());
+        if ($petugas) {
+            return redirect()->route('admin.petugas.index')->with('success', 'Petugas berhasil ditambahkan');
+        }
+
+        return redirect()->route('admin.petugas.index')->with('error', 'Petugas gagal ditambahkan');
     }
 
-    /**
-     * Fungsi untuk menampilkan halaman edit petugas dan mengirimkan data petugas berdasarkan id beserta data relasi pengguna
-     * @param mixed $id
-     * @return \Illuminate\Contracts\View\View
-     */
-    public function edit($id): View
+    public function edit(int $id): View
     {
         $petugas = Petugas::with('pengguna')->findOrFail($id);
         $pengguna = Pengguna::all();
@@ -96,26 +73,17 @@ class PetugasController extends Controller
         ]);
     }
 
-    /**
-     * Fungsi untuk mengupdate data petugas
-     * @param \App\Http\Requests\PetugasRequest $request
-     * @param mixed $id
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function update(PetugasRequest $request, $id): RedirectResponse
+    public function update(PetugasRequest $request, int $id): RedirectResponse
     {
         $petugas = Petugas::findOrFail($id);
-        $petugas->update($request->only(['pengguna_id', 'nama_petugas', 'jabatan', 'tanggal_bergabung']));
-        return redirect()->route('admin.petugas.index')->with('success', 'Petugas berhasil diperbarui');
+        if ($petugas->update($request->validated())) {
+            return redirect()->route('admin.petugas.index')->with('success', 'Petugas berhasil diperbarui');
+        }
+
+        return redirect()->route('admin.petugas.index')->with('error', 'Petugas gagal diperbarui');
     }
 
-
-    /**
-     * Fungsi untuk menghapus data petugas
-     * @param mixed $id
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function destroy($id): RedirectResponse
+    public function destroy(int $id): RedirectResponse
     {
         $petugas = Petugas::findOrFail($id);
         $petugas->delete();
